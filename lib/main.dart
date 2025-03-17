de565ed391941861e196d7b92716fdba5b24509b
bensalcie <bensalcie@gmail.com>
2025-03-17 21:30:35 +0300

    Includes App Global Observer, Includes Dependancies initialization on Main , Adds DI

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:chatwithbloc/app.dart';
+import 'package:chatwithbloc/core/di/injection.dart';
+import 'package:chatwithbloc/core/utils/app_global_observer.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  if (kDebugMode) {
+    await dotenv.load(fileName: ".dev.env");
+    Bloc.observer = AppGlobalBlocObserver();
+  } else {
+    await dotenv.load(fileName: ".env");
+  }
+  await configureDependencies();
+
   runApp(const MyApp());
 }
-
-
